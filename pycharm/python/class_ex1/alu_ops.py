c95912a583b92b74456b735af92a4f2ed7b24067
sriharshduppalli <sriharshaec14@gmail.com>
2022-02-12 21:05:25 +0530

    alu operations example class

---
+++ pycharm/python/class_ex1/alu_ops.py
@@ -0,0 +1,28 @@
+class Aluops:
+    num1 = 0
+    num2 = 0
+    ans = 0
+
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+
+    def calculate(self, ops):
+        if ops == "add":
+            self.ans = self.num1 + self.num2
+        if ops == "sub":
+            self.ans = self.num1 - self.num2
+        if ops == "mul":
+            self.ans = self.num1 * self.num2
+
+        print("{} of two numbers = " .format(ops) + str(self.ans))
+
+
+obj = Aluops(10, 20)
+obj.calculate("add")
+
+obj = Aluops(1000, 20000)
+obj.calculate("sub")
+
+obj = Aluops(50, 20)
+obj.calculate("mul")
