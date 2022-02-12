ddb93cc24a966f4191665a10bdba392f983a5a3d
sriharshduppalli <sriharshaec14@gmail.com>
2022-02-12 20:50:24 +0530

    add class example

---
+++ pycharm/python/class_ex1/addition.py
@@ -0,0 +1,19 @@
+class Addition:
+    num1 = 0
+    num2 = 0
+    ans = 0
+
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+
+    def calculate(self):
+        self.ans = self.num1 + self.num2
+        print("addition of two numbers = " + str(self.ans))
+
+
+obj = Addition(10, 20)
+obj.calculate()
+
+obj = Addition(1000, 20000)
+obj.calculate()
